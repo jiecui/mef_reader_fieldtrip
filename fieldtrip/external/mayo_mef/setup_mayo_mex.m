0f0a8d78ac98c97206f88a80d1897fed40cdcb64
Richard Jie Cui <richard.cui@utoronto.ca>
2023-01-30 22:03:17 -0600

    checked mex files

--- fieldtrip/external/mayo_mef/setup_mayo_mex.m
+++ fieldtrip/external/mayo_mef/setup_mayo_mex.m
@@ -76,6 +76,10 @@ function setup_mayo_mex(options)
         med_mex_path = string(1, 0);
     end % if
 
+    % install mayo_mef package
+    % ------------------------
+    ft_hastoolbox('mayo_mef', 1)
+
     % get current directory
     % ---------------------
     cur_dir = pwd;
@@ -91,7 +95,7 @@ function setup_mayo_mex(options)
         cd([mayo_mef, filesep, 'mex_mef'])
         make_mex_mef
     else % check mex files in mayo_mef
-        valid_mex = check_mex_files(mayo_mef);
+        valid_mex = check_mex_files(mayo_mef, MexType = "MEF");
 
         if valid_mex == false
             cd([mayo_mef, filesep, 'mex_mef'])
@@ -102,9 +106,18 @@ function setup_mayo_mex(options)
 
     % check MED mex binary
     % --------------------
-    fprintf('Setting up MED mex binary...\n')
+    if ~isempty(med_mex_path)
+        fprintf('Setting up MED mex binary...\n')
 
-    % TODO: check if MED mex files are valid
+        % check mex files in read_MED
+        valid_mex = check_mex_files(med_mex_path, MexType = 'MED');
+
+        if valid_mex == false
+            ft_warning('MAYO_MEF:setup_mayo_mex', ...
+            'None or not all MED mex files can be found. Please install read_MED package (http://darkhorseneuro.com) or manually set DHN root directory\n')
+        end % if
+
+    end % if
 
     % return to original directory
     % ----------------------------
@@ -115,12 +128,49 @@ end
 % ==========================================================================
 % subroutines
 % ==========================================================================
-function valid_mex = check_mex_files(mex_path)
+function valid_mex = check_mex_files(mex_path, options)
     % SETUP_MAYO_MEX.CHECK_MEX_FILES check if mex files are valid
 
     arguments
         mex_path (1, 1) string % full path to mex files
     end % positional
+
+    arguments
+        options.MexType (1, 1) string {mustBeMember(options.MexType, ["MEF", "MED"])} ...
+            = "MEF" % MEF or MED
+    end % optional
+
+    mex_type = options.MexType;
+
+    switch mex_type
+        case 'MEF'
+            valid_mex_1 = isfile(fullfile(mex_path + "read_mef_header_2p1" + "."+mexext));
+            valid_mex_2 = isfile(fullfile(mex_path + "decompress_mef_2p1" + "."+mexext));
+            valid_mex_3 = isfile(fullfile(mex_path + "read_mef_session_metadata" + "."+mexext));
+            valid_mex_4 = isfile(fullfile(mex_path + "read_mef_ts_data" + "."+mexext));
+
+            valid_mex = valid_mex_1 && valid_mex_2 && valid_mex_3 && valid_mex_4;
+
+            if valid_mex
+                fprintf('MEF mex files are found.\n')
+            else
+                ft_warning('None or not all MEF mex files can be found.\n')
+            end % if
+
+        case 'MED'
+            valid_mex_1 = isfile(fullfile(mex_path + "load_session" + "."+mexext));
+            valid_mex_2 = isfile(fullfile(mex_path + "matrix_MED_exec" + "."+mexext));
+            valid_mex_3 = isfile(fullfile(mex_path + "read_MED_exec" + "."+mexext));
+
+            valid_mex = valid_mex_1 && valid_mex_2 && valid_mex_3;
+
+            if valid_mex
+                fprintf('MED mex files are found.\n')
+            else
+                ft_warning('None or not all MED mex files can be found.\n')
+            end % if
+
+    end % switch
 
 end % function
 

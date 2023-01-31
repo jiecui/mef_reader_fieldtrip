83f1619eabc9fab38a8b95759e36e032f9fe2ce3
Richard Jie Cui <richard.cui@utoronto.ca>
2023-01-30 22:02:52 -0600

    added new files

---
+++ fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData/MultiscaleElectrophysiologyData.m
@@ -0,0 +1,26 @@
+classdef MultiscaleElectrophysiologyData < handle
+    % Class MULTISCALEELECTROPHYSIOLOGYDATA process MED channel data
+
+    % Copyright 2023 Richard J. Cui. Created: Mon 01/30/2023 10:01:06.104 PM
+    % $Revision: 0.1 $  $Date: Mon 01/30/2023 10:01:06.111 PM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+    properties
+
+    end % properties
+
+    methods
+
+        function this = MultiscaleElectrophysiologyData()
+
+        end
+
+    end % methods
+
+end % classdef
+
+% [EOF]

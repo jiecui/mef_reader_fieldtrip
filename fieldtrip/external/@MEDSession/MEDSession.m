a67c0058dcde181be5bd3cb266b4174a04356aa9
Richard Jie Cui <richard.cui@utoronto.ca>
2023-02-12 23:23:44 -0600

    added MEDSession.m

---
+++ fieldtrip/external/@MEDSession/MEDSession.m
@@ -0,0 +1,35 @@
+classdef MEDSession < handle
+    % Class MEDSESSION processes and stores data from a MED session.
+    % 
+    % Syntax:
+    %   this = MEDSession;
+    %
+    % Input(s):
+    % 
+    % Output(s):
+    %
+    % See also .
+
+    % Copyright 2023 Richard J. Cui. Created: Sun 02/12/2023  9:38:14.305 PM
+    % $Revision: 0.1 $  $Date: Sun 02/12/2023  9:38:14.306 PM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+    properties
+
+    end % properties
+
+    methods
+
+        function this = MEDSession()
+
+        end
+
+    end % methods
+
+end % classdef
+
+% [EOF]

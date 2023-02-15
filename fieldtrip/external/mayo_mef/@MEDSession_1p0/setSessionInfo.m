dc13fbe90ccc09a22a67fb7d603b8740e16d3c0a
Richard Jie Cui <richard.cui@utoronto.ca>
2023-02-14 23:41:20 -0600

    added setSessionInfo

---
+++ fieldtrip/external/mayo_mef/@MEDSession_1p0/setSessionInfo.m
@@ -0,0 +1,30 @@
+function metadata = setSessionInfo(this, sesspath, password, sort_channel)
+    % MEDSESSION_1P0.SETSESSIONINFO Set session information of MEDSession_1p0
+    %
+    % Syntax:
+    %   setSessionInfo(this, sesspath, password)
+    %   setSessionInfo(__, sort_channel)
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
+
+    % Copyright 2023 Richard J. Cui. Created: Tue 02/14/2023 11:36:09.573 PM
+    % $Revision: 0.1 $  $Date: Tue 02/14/2023 11:36:09.587 PM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+end % function setSessionInfo
+
+% [EOF]

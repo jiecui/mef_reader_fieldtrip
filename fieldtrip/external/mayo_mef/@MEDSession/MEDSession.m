9db0cd148589da2631bf8ff46014be24bf511f91
Richard Jie Cui <richard.cui@utoronto.ca>
2023-02-21 00:38:52 -0600

    added MEDSession class

---
+++ fieldtrip/external/mayo_mef/@MEDSession/MEDSession.m
@@ -0,0 +1,26 @@
+classdef MEDSession < handle
+    % Class MEDSESSION processes MED session data.
+
+    % Copyright 2023 Richard J. Cui. Created: Tue 02/21/2023 12:21:48.365 AM
+    % $Revision: 0.1 $  $Date: Tue 02/21/2023 12:21:48.371 AM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+    properties
+
+    end % properties
+
+    methods
+
+        function this = MEDSession()
+
+        end
+
+    end % methods
+
+end % classdef
+
+% [EOF]

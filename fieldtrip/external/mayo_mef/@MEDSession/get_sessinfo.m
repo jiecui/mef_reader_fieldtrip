db37409113000ae44a3ca2b49eb7db981e74290f
Richard Jie Cui <richard.cui@utoronto.ca>
2023-02-21 23:31:28 -0600

    added

---
+++ fieldtrip/external/mayo_mef/@MEDSession/get_sessinfo.m
@@ -0,0 +1,28 @@
+function varargout = get_sessinfo(this)
+    % MEDSESSION.GET_SESSINFO (summary)
+    %
+    % Syntax:
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
+
+    % Copyright 2023 Richard J. Cui. Created: Tue 02/21/2023 11:21:04.215 PM
+    % $Revision: 0.1 $  $Date: Tue 02/21/2023 11:21:04.228 PM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+end % function get_sessinfo
+
+% [EOF]

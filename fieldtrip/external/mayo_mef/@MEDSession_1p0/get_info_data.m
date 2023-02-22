42fbbfccfb75571fabfccbc7dfacf6684cc81bbe
Richard Jie Cui <richard.cui@utoronto.ca>
2023-02-21 23:32:10 -0600

    added

---
+++ fieldtrip/external/mayo_mef/@MEDSession_1p0/get_info_data.m
@@ -0,0 +1,28 @@
+function [sess_info, unit] = get_info_data(this)
+    % MEDSESSION_1P0.GET_INFO_DATA (summary)
+    %
+    % Syntax:
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
+
+    % Copyright 2023 Richard J. Cui. Created: Tue 02/21/2023 11:27:28.805 PM
+    % $Revision: 0.1 $  $Date: Tue 02/21/2023 11:27:28.806 PM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+end % function get_info_data
+
+% [EOF]

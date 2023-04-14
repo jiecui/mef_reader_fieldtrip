12291d95af62c678433498bab041f5f4404a5ad4
Richard Jie Cui <richard.cui@utoronto.ca>
2023-04-14 00:14:14 -0500

    added analyzeContinuity

---
+++ fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData_1p0/analyzeContinuity.m
@@ -0,0 +1,28 @@
+function seg_cont = analyzeContinuity(this)
+    % ANALYZECONTINUITY (summary)
+    %
+    % Syntax:
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
+
+    % Copyright 2023 Richard J. Cui. Created: Fri 04/14/2023 12:11:12.027 AM
+    % $Revision: 0.1 $  $Date: Fri 04/14/2023 12:11:12.033 AM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+end % function analyzeContinuity
+
+% [EOF]

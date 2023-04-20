b05cf8cb7dc82ae3cdaf495f581a1563cd7c02ee
Richard Jie Cui <richard.cui@utoronto.ca>
2023-04-20 00:30:49 -0500

    added importSignal

---
+++ fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData_1p0/importSignal.m
@@ -0,0 +1,36 @@
+function [x, t] = importSignal(this)
+    % MULTISCALEELECTROPHYSIOLOGYDATA_1P0.IMPORTSIGNAL imports MED 1.0 signal into MATLAB
+    %
+    % Syntax:
+    %   [x, t] = importSignal(this)
+    %   [x, t] = importSignal(__, start_end)
+    %   [x, t] = importSignal(__, start_end, st_unit)
+    %   [x, t] = importSignal(__, start_end, st_unit, filepath)
+    %   [x, t] = importSignal(__, start_end, st_unit, filepath, filename)
+    %   [x, t] = importSignal(__, 'Level1Password', level_1_pw)
+    %   [x, t] = importSignal(__, 'Level2Password', level_2_pw)
+    %   [x, t] = importSignal(__, 'AccessLevel', access_level)
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
+
+    % Copyright 2023 Richard J. Cui. Created: Thu 04/20/2023 12:24:33.818 AM
+    % $Revision: 0.1 $  $Date: Thu 04/20/2023 12:24:33.825 AM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+end % function importSignal
+
+% [EOF]

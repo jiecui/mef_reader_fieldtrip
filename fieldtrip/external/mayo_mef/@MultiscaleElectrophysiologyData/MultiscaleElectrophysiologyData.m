744222499209aae4cbb96ca96360b89d3f0ac237
Richard Jie Cui <richard.cui@utoronto.ca>
2023-06-28 01:25:48 -0500

    updated properties

--- fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData/MultiscaleElectrophysiologyData.m
+++ fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData/MultiscaleElectrophysiologyData.m
@@ -2,7 +2,7 @@ classdef MultiscaleElectrophysiologyData < handle
     % Class MULTISCALEELECTROPHYSIOLOGYDATA process MED channel data
 
     % Copyright 2023 Richard J. Cui. Created: Mon 01/30/2023 10:01:06.104 PM
-    % $Revision: 0.2 $  $Date: Thu 04/13/2023  1:00:43.288 AM $
+    % $Revision: 0.3 $  $Date: Wed 06/28/2023  1:24:40.283 AM $
     %
     % Rocky Creek Dr. NE
     % Rochester, MN 55906, USA
@@ -26,6 +26,7 @@ classdef MultiscaleElectrophysiologyData < handle
         FileName % [str] filename of MED channel file including ext
         Continuity % [table] data segments of conituous sampling (see
         % analyzeContinuity.m for the detail)
+        ContinuityCorrected % [table] data segments of conituous sampling corrected
         ChanSamplingFreq % sampling frequency of channel (Hz)
         SampleTimeInterval % sample time interval = [lower, upper] (uUTC),
         % indicating the lower and upper bound of the time interval between

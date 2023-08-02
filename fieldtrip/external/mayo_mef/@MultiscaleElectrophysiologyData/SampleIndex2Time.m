ce5a440cf6870fe208144a6ae30d3eba413ce07c
Richard Jie Cui <richard.cui@utoronto.ca>
2023-08-01 23:47:47 -0500

    added sampleindex2time fun

---
+++ fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData/SampleIndex2Time.m
@@ -0,0 +1,47 @@
+function [sample_time, sample_yn] = SampleIndex2Time(this, sample_index, options)
+    % MULTISCALEELECTROPHYSILOGYDATA.SAMPLEINDEX2TIME convert sample index to sample time
+    %
+    % Syntax:
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
+
+    % Copyright 2023 Richard J. Cui. Created: Tue 08/01/2023 11:35:13.940 PM
+    % $Revision: 0.1 $  $Date: Tue 08/01/2023 11:35:13.946 PM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+    % ======================================================================
+    % parse inputs
+    % ======================================================================
+    arguments
+        this (1, 1) MultiScaleElectrophysiologyData
+        sample_index (:, 1) double
+    end % positional
+
+    arguments
+        options.st_unit (1, :) char ...
+            {mustBeMember(options.st_unit, {'uutc', 'msec', 'second', 'minute', 'hour', 'day'})} = 'uutc'
+    end % name-value
+
+    st_unit = options.st_unit;
+
+    % ======================================================================
+    % main
+    % ======================================================================
+
+end % function SampleIndex2Time
+
+% [EOF]

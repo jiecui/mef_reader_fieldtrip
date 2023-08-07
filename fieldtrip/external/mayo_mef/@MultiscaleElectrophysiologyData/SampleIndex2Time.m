7f04707a572309733f2c880071ae2d68fc5da79c
Richard Jie Cui <richard.cui@utoronto.ca>
2023-08-07 00:28:50 -0500

    updated sorting sample time

--- fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData/SampleIndex2Time.m
+++ fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData/SampleIndex2Time.m
@@ -43,10 +43,47 @@ function [sample_time, sample_yn] = SampleIndex2Time(this, sample_index, options
     % ======================================================================
     % set parameters
     % --------------
-    sample_time=zeros(size(sample_index));
+    sample_time = zeros(size(sample_index));
     sample_yn = false(size(sample_index));
+    [sorted_si, orig_idx] = sort(sample_index); % sort sample index
 
+    if isempty(this.ContinuityCorrected)
+        cont = this.analyzeContinuity;
+    else
+        cont = this.ContinuityCorrected;
+    end % if
+
+    fs = this.ChanSamplingFreq;
+
+    [sorted_st, sorted_st_yn] = findSampleTime(fs, cont, fs, sorted_si); % TODO
+
+    % convert to desired unit
+    % -----------------------
+    switch st_unit
+        case 'uutc'
+            sorted_sample_time = sorted_st;
+        case 'msec'
+            sorted_sample_time = sorted_st / 1e3;
+        case 'second'
+            sorted_sample_time = sorted_st / 1e6;
+        case 'minute'
+            sorted_sample_time = sorted_st / 1e6/60;
+        case 'hour'
+            sorted_sample_time = sorted_st / 1e6/3600;
+        case 'day'
+            sorted_sample_time = sorted_st / 1e6/3600/24;
+    end % switch-case
+
+    % output
+    % ------
+    sample_time(orig_idx) = sorted_sample_time;
+    sample_yn(orig_idx) = sorted_st_yn;
 
 end % function SampleIndex2Time
+
+% ==========================================================================
+% subroutines
+% ==========================================================================
+
 
 % [EOF]

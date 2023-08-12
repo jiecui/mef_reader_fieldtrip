55352db0262854a3cd840cd78db0494da694119f
Richard Jie Cui <richard.cui@utoronto.ca>
2023-08-12 00:10:47 -0500

    updated sample intex to time

--- fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData/SampleIndex2Time.m
+++ fieldtrip/external/mayo_mef/@MultiscaleElectrophysiologyData/SampleIndex2Time.m
@@ -16,7 +16,7 @@ function [sample_time, sample_yn] = SampleIndex2Time(this, sample_index, options
     % See also .
 
     % Copyright 2023 Richard J. Cui. Created: Tue 08/01/2023 11:35:13.940 PM
-    % $Revision: 0.1 $  $Date: Tue 08/01/2023 11:35:13.946 PM $
+    % $Revision: 0.2 $  $Date: Sat 08/12/2023 12:08:22.300 AM $
     %
     % Rocky Creek Dr. NE
     % Rochester, MN 55906, USA
@@ -55,7 +55,7 @@ function [sample_time, sample_yn] = SampleIndex2Time(this, sample_index, options
 
     fs = this.ChanSamplingFreq;
 
-    [sorted_st, sorted_st_yn] = findSampleTime(fs, cont, fs, sorted_si); % TODO
+    [sorted_st, sorted_st_yn] = findSampleTime(fs, cont, sorted_si); % TODO
 
     % convert to desired unit
     % -----------------------
@@ -84,6 +84,41 @@ end % function SampleIndex2Time
 % ==========================================================================
 % subroutines
 % ==========================================================================
+function [sample_time, sample_yn] = findSampleTime(fs, cont, sample_index)
+    % FINDSAMPLETIME find sample time from sample index
 
+    arguments
+        fs (1, 1) double % sampling frequency
+        cont (:, :) table % continuity table
+        sample_index (:, 1) double
+    end % positional
+
+    % set parameters
+    % --------------
+    num_si = numel(sample_index);
+    sample_time = zeros(size(sample_index));
+    sample_yn = false(size(sample_index));
+
+    % find sample time
+    % ----------------
+    % find sample time for each sample index
+    for k = 1:num_si
+        % find the continuity table row that contains the sample index
+        idx_k = cont.start_index <= sample_index(k) & cont.end_index >= sample_index(k);
+        cont_row = cont(idx_k, :);
+
+        if isempty(cont_row)
+            sample_time(k) = NaN;
+            sample_yn(k) = false;
+            continue
+        end % if
+
+        % find the sample index in the continuity table
+        delta_microsec = (sample_index(k) - cont_row.start_index) * 1e6 / fs;
+        sample_time(k) = cont_row.start_time + delta_microsec;
+        sample_yn(k) = true;
+    end % for
+
+end % function findSampleTime
 
 % [EOF]

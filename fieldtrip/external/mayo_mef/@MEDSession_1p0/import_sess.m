9696caef35de5ee88300d4279f6c64b29185047d
Richard Jie Cui <richard.cui@utoronto.ca>
2023-09-14 23:59:21 -0500

    added import_sess

---
+++ fieldtrip/external/mayo_mef/@MEDSession_1p0/import_sess.m
@@ -0,0 +1,30 @@
+function [X, t] = import_sess(this, begin_stop, bs_unit, sel_chan, options)
+    % MEDSESSION_1P0.IMPORT_SESS import session of MED 1.0 data
+    %
+    % Syntax:
+    %   [X, t] = import_sess(this, begin_stop, bs_unit, sel_chan)
+    %   [X, t] = import_sess(__, pw)
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
+
+    % Copyright 2023 Richard J. Cui. Created: Thu 09/14/2023 11:56:16.600 PM
+    % $Revision: 0.1 $  $Date: Thu 09/14/2023 11:56:16.657 PM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
+
+end % function import_sess
+
+% [EOF]

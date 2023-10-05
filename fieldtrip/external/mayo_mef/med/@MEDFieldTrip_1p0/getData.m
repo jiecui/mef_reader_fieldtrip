462a3f613e219124d58bfab4bf843e40a6534c2d
Richard Jie Cui <richard.cui@utoronto.ca>
2023-10-05 00:11:24 -0500

    updated getData

--- fieldtrip/external/mayo_mef/med/@MEDFieldTrip_1p0/getData.m
+++ fieldtrip/external/mayo_mef/med/@MEDFieldTrip_1p0/getData.m
@@ -1,27 +1,27 @@
-function getData
-% GETDATA (summary)
-%
-% Syntax:
-%
-% Input(s):
-%
-% Output(s):
-%
-% Example:
-%
-% Note:
-%
-% References:
-%
-% See also .
+function dat = getData(this, channames, sampleunit, begsample, endsample, chanindx)
+    % MEDFILEDTRIP_1P0.GETDATA read data from MED 1.0 dataset for FieldTrip
+    %
+    % Syntax:
+    %
+    % Input(s):
+    %
+    % Output(s):
+    %
+    % Example:
+    %
+    % Note:
+    %
+    % References:
+    %
+    % See also .
 
-% Copyright 2023 Richard J. Cui. Created: Tue 10/03/2023 12:25:01.918 AM
-% $Revision: 0.1 $  $Date: Tue 10/03/2023 12:25:01.938 AM $
-%
-% Rocky Creek Dr. NE
-% Rochester, MN 55906, USA
-%
-% Email: richard.cui@utoronto.ca
+    % Copyright 2023 Richard J. Cui. Created: Tue 10/03/2023 12:25:01.918 AM
+    % $Revision: 0.1 $  $Date: Tue 10/03/2023 12:25:01.938 AM $
+    %
+    % Rocky Creek Dr. NE
+    % Rochester, MN 55906, USA
+    %
+    % Email: richard.cui@utoronto.ca
 
 end % function getData
 

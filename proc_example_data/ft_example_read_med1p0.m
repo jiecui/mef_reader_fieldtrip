3a22653fb4f76bce3c77c7c19a7637becbed5af5
Richard Jie Cui <richard.cui@utoronto.ca>
2023-10-11 00:18:22 -0500

    fixed bugs for example code

--- proc_example_data/ft_example_read_med1p0.m
+++ proc_example_data/ft_example_read_med1p0.m
@@ -34,70 +34,62 @@ fs = med_ft.SamplingFrequency;
 
 figure
 ph = plot(t / fs, X);
-legend(ph, med_ft.SelectedChannel, 'Interpreter','none')
+legend(ph, med_ft.SelectedChannel, 'Interpreter', 'none')
 xlim([0, 1]) % zoom into the 1st second data
 xlabel('Time (s)')
 ylabel('Amplitude')
+title('MED 1.0 data read by MEDFieldTrip_1p0', 'Interpreter', 'none')
 
 %% read the MED 1.0 data using FieldTrip routines
 % read data header with ft_read_header()
 hdr = ft_read_header(sesspath, 'password', password);
 
-% read a specific channel with ft_read_data()
-% chpath = fullfile(sesspath, [hdr.label{1}, '.ticd']);
-% x = ft_read_data(chpath, 'begsample', 1, 'endsample', 4000, 'header', hdr, ...
-%     'password', password, 'chanindx', 1); % don't ommit 'chanindx'
-% figure
-% plot(x)
-% xlim([1 4000])
-% xlabel('Time (sample index)')
-% legend(hdr.label{1})
-
-%% read data with ft_read_data() but specifying time interval using seconds
+% read data with ft_read_data() but specifying time interval using seconds
 % Let's import 10 seconds data at the beginning of the recording
-% in_unit = 'second';
-% be_second = [0, 10]; % 10-second time of data from the start
-% out_unit = 'index';
-% be_sample = med_ft.SessionUnitConvert(be_second, in_unit, out_unit);
-% dat = ft_read_data(sesspath, ...
-%     'begsample', be_sample(1), ...
-%     'endsample', be_sample(2), ...
-%     'header', hdr, ...
-%     'password', password, ...
-%     'chanindx', [1 2]); % the order of read data can be decided by
-% % key-value 'chanindx'
+in_unit = 'second';
+be_second = [0, 10]; % 10-second time of data from the start
+out_unit = 'index';
+be_sample = med_ft.SessionUnitConvert(be_second, InUnit = in_unit, OutUnit = out_unit);
+dat = ft_read_data(sesspath, ...
+    'begsample', be_sample(1), ...
+    'endsample', be_sample(2), ...
+    'header', hdr, ...
+    'password', password, ...
+    'chanindx', [1 2]); % the order of read data can be decided by
+% key-value 'chanindx'
 
-% t = linspace(be_second(1), be_second(2), be_sample(2) - be_sample(1) + 1);
-% figure
-% plot(t, dat')
-% xlim([0 1.5] + be_second(1))
-% xlabel('Time (s)')
-% legend(hdr.label{1}, hdr.label{2})
+t = linspace(be_second(1), be_second(2), be_sample(2) - be_sample(1) + 1);
+figure
+plot(t, dat')
+xlim([0 1] + be_second(1)) % zoom into the 1st second data
+xlabel('Time (s)')
+legend(hdr.label{1}, hdr.label{2}, 'interpreter', 'none')
+title('MED 1.0 data read by ft_read_data()', 'Interpreter', 'none')
 
 %% read data with ft_preprocessing()
 % Let's import 5 trials/epochs. Each trial is 1.50 second long.  The
 % trigger time of the 5 trials are at 0.5, 2.0, 3.5, 5.0 and 6.5 seconds,
 % with the pre-stimulus length of 0.5 second.
-%
+
 % setup trial information
-% trig = [.5, 2, 3.5, 5, 6.5]; % in seconds
-% n_trig = length(trig); % number of triggers
-% trigger = med_ft.SessionUnitConvert(trig, in_unit, out_unit)';
-% prestim = med_ft.SessionUnitConvert(.5, in_unit, out_unit) * ones(n_trig, 1);
-% poststim = med_ft.SessionUnitConvert(1., in_unit, out_unit) * ones(n_trig, 1);
-% trl = [trigger - prestim + 1, trigger + poststim, -prestim];
+trig = [.5, 2, 3.5, 5, 6.5]; % in seconds
+n_trig = length(trig); % number of triggers
+trigger = med_ft.SessionUnitConvert(trig, InUnit = in_unit, OutUnit = out_unit)';
+prestim = med_ft.SessionUnitConvert(.5, InUnit = in_unit, OutUnit = out_unit) * ones(n_trig, 1);
+poststim = med_ft.SessionUnitConvert(1., InUnit = in_unit, OutUnit = out_unit) * ones(n_trig, 1);
+trl = [trigger - prestim + 1, trigger + poststim, -prestim];
 
-% % read the data
-% cfg = [];
-% cfg.dataset = sesspath;
-% cfg.password = password;
-% cfg.header = hdr;
-% cfg.trl = trl;
-% dat_ieeg = ft_preprocessing(cfg);
+% read the data
+cfg = [];
+cfg.dataset = sesspath;
+cfg.password = password;
+cfg.header = hdr;
+cfg.trl = trl;
+dat_ieeg = ft_preprocessing(cfg);
 
-% % plot the data
-% cfg.viewmode = 'vertical';
-% brwview = ft_databrowser(cfg, dat_ieeg);
+% plot the data
+cfg.viewmode = 'vertical';
+brwview = ft_databrowser(cfg, dat_ieeg);
 
 %% Copyright (c) 2020 MNL group
 % Created on Sun 03/22/2020  9:03:27.318 PM
